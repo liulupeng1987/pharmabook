d756b6ce084ef0402332de3982b6055585a85a19
liulupeng <liulupeng3385@163.com>
2017-10-08 23:24:26 +0800

    finish api seed supplement

--- app/controllers/medicine_manufacturers_controller.rb
+++ app/controllers/medicine_manufacturers_controller.rb
@@ -2,6 +2,7 @@ class MedicineManufacturersController < ApplicationController
   before_action :authenticate_user! , only: [:new]
   def show
     @medicine_manufacturer = MedicineManufacturer.find(params[:id])
+    @apis = @medicine_manufacturer.apis.paginate(:page => params[:page], :per_page => 20)
     @medicines = @medicine_manufacturer.medicines.paginate(:page => params[:page], :per_page => 20)
   end
 
@@ -12,9 +13,9 @@ class MedicineManufacturersController < ApplicationController
 
   def search
     if params[:search].present?
-      @medicine_manufacturers = MedicineManufacturer.search(params[:search], fields:["name", "contact_info"], page: params[:page], per_page: 20)
+      @medicine_manufacturers = MedicineManufacturer.search(params[:search], fields:["name", "contact_info"], order: {"id" => "ASC"}, page: params[:page], per_page: 20)
     else
-      @medicine_manufacturers = MedicineManufacturer.all.paginate(:page => params[:page], :per_page => 20)
+      @medicine_manufacturers = MedicineManufacturer.all.order('id ASC').paginate(:page => params[:page], :per_page => 20)
     end
   end
 
