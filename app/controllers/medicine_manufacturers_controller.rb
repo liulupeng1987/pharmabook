d3c27aded8e7ef3f1a70417b0cb31ae1832e6a66
liulupeng <liulupeng3385@163.com>
2017-10-17 21:34:27 +0800

    remove order of manufacturer search

--- app/controllers/medicine_manufacturers_controller.rb
+++ app/controllers/medicine_manufacturers_controller.rb
@@ -13,9 +13,9 @@ class MedicineManufacturersController < ApplicationController
 
   def search
     if params[:search].present?
-      @medicine_manufacturers = MedicineManufacturer.search(params[:search], fields:["name", "contact_info"], order: {"id" => "ASC"}, page: params[:page], per_page: 20)
+      @medicine_manufacturers = MedicineManufacturer.search(params[:search], fields:["name", "contact_info"], page: params[:page], per_page: 20)
     else
-      @medicine_manufacturers = MedicineManufacturer.all.order('id ASC').paginate(:page => params[:page], :per_page => 20)
+      @medicine_manufacturers = MedicineManufacturer.all.paginate(:page => params[:page], :per_page => 20)
     end
   end
 
