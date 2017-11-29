b571faa570a293d09b5c5f39413118d4241f1d46
liulupeng <liulupeng3385@163.com>
2017-11-29 22:14:50 +0800

    finish filter function

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -3,6 +3,16 @@ class CertificatesController < ApplicationController
 
   def index
     @certificates = Certificate.all.paginate(:page => params[:page], :per_page => 20)
+
+  if params[:filter] == "fda"
+      @certificates = Certificate.where.not(:fda => nil).paginate(:page => params[:page], :per_page => 20)
+    elsif params[:filter] == "eu"
+      @certificates = Certificate.where.not(:eu => nil).paginate(:page => params[:page], :per_page => 20)
+    elsif params[:filter] == "who"
+      @certificates = Certificate.where.not(:who => nil).paginate(:page => params[:page], :per_page => 20)
+    elsif params[:filter] == "pics"
+      @certificates = Certificate.where.not(:pics => nil).paginate(:page => params[:page], :per_page => 20)
+    end
   end
 
 
@@ -10,8 +20,10 @@ class CertificatesController < ApplicationController
       if params[:search].present?
 
         @certificates = Certificate.search(params[:search], fields:["manufacturer", "fda", "eu", "who", "pics"], order: {id: :asc}, page: params[:page], per_page: 20)
+
       else
         @certificates = Certificate.all.order("created_at ASC").paginate(:page => params[:page], :per_page => 20)
       end
+
     end
 end
